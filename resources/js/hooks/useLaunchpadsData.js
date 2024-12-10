1b7f98891b73ac7097172cf78257f313ebdcf0c0
Zamba Izo <ofuzak@gmail.com>
2024-12-10 03:16:48 -0500

    fix launchpads

--- resources/js/hooks/useLaunchpadsData.js
+++ resources/js/hooks/useLaunchpadsData.js
@@ -3,27 +3,30 @@ import { ref, watch } from "vue";
 
 import { get } from "@vueuse/core";
 import { getPublicClient } from '@wagmi/core';
-import { useConfig } from "@wagmi/vue";
+import { useChainId, useConfig } from "@wagmi/vue";
 import { formatEther } from "viem";
 
 import { multicall } from "./multicall";
 export const useLaunchpadsData = (launchpads, rates) => {
     const stakes = ref([...get(launchpads)]);
     const loading = ref(false);
-    const client = getPublicClient(useConfig());
+    const chainId = useChainId();
+    const config = useConfig();
     const update = async () => {
+        const client = getPublicClient(config);
         stakes.value = [];
         loading.value = true;
         const calls = get(launchpads).map(s => {
             return {
                 ...s,
-                contract: { abi: s.factory.abi, address: s.contract },
+                contract: { abi: s.factory.abi, address: s.contract, account: '0xDF473E86eC9800A8589C2977121584d199129952' },
                 currentPhase: { functionName: 'currentPhase', args: [] },
                 isFinalized: { functionName: 'isFinalized', args: [] },
                 totalETHCollected: { functionName: 'totalETHCollected', args: [] },
                 getBondingCurveSettings: { functionName: 'getBondingCurveSettings', args: [] }
             };
         });
+        console.log(calls);
         const [results] = await multicall([calls], { client }).catch(e => {
             console.log(e);
             return [];
@@ -31,8 +34,8 @@ export const useLaunchpadsData = (launchpads, rates) => {
         console.log(results);
         stakes.value = results.map(r => {
             const rate = rates[r.chainId] ?? 1;
-            const totalETHCollected = formatEther(r.totalETHCollected);
-            const targetEther = formatEther(r.getBondingCurveSettings.bondingTarget);
+            const totalETHCollected = formatEther(r?.totalETHCollected ?? '0');
+            const targetEther = formatEther(r.getBondingCurveSettings?.bondingTarget ?? '0');
             return {
                 ...r,
                 status: r.currentPhase === 0 ? 'prebond' : r.currentPhase === 1 ? 'bonding' : 'finalized',
@@ -45,7 +48,10 @@ export const useLaunchpadsData = (launchpads, rates) => {
         loading.value = false;
         return stakes.value;
     };
-    watch(launchpads, update, { immediate: true });
+    watch([launchpads, chainId], ([launchpads, chainId]) => {
+        if (chainId)
+            update();
+    });
     return {
         launchpads: stakes,
         update,
