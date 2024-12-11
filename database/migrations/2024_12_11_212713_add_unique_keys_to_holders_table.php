7093b897bfc677d41655b2744f46004afbd9aa24
Zamba Izo <ofuzak@gmail.com>
2024-12-11 16:32:08 -0500

    Fix the unique key migraion

--- database/migrations/2024_12_11_212713_add_unique_keys_to_holders_table.php
+++ database/migrations/2024_12_11_212713_add_unique_keys_to_holders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,8 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Truncate the table first
+        DB::table('holders')->truncate();
+
         Schema::table('holders', function (Blueprint $table) {
-            //
             $table->unique(['launchpad_id', 'address']);
         });
     }
@@ -23,7 +26,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('holders', function (Blueprint $table) {
-            //
             $table->dropUnique(['launchpad_id', 'address']);
         });
     }
